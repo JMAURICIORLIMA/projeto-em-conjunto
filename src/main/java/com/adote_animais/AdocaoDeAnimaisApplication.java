510c71b85bcf6f68232db17c30f92f47c5cb6383
José Maurício <mauricio.rodriguesdelima@gmail.com>
2024-08-27 15:42:54 -0300

    Initial commit

---
+++ src/main/java/com/adote_animais/AdocaoDeAnimaisApplication.java
@@ -0,0 +1,13 @@
+package com.adote_animais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoDeAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoDeAnimaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adote_animais/AdocaoDeAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adote_animais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoDeAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
